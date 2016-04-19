a78159f1fa0b85df26a6ac25e23cd78d891d1a48
Mikhail Markine <markine@users.noreply.github.com>
2016-04-19 15:00:18 -0700

    Add checks for GET request types

--- tests/test_wellknown.py
+++ tests/test_wellknown.py
@@ -2,15 +2,28 @@ from django.test import TestCase
 from wellknown import models
 
 
+VALID_PATH = 'acme-challenge/challenge-request'
+MISSING_PATH = 'missing'
+
+
 class WellKnownIntegrationTest(TestCase):
 
-    def testFetchAcmeChallenge(self):
-        PATH = 'acme-challenge/challenge-request'
+    def testFetchValidPath(self):
         DATA = 'challenge-response'
         models.Resource(
-            path=PATH,
+            path=VALID_PATH,
             content=DATA).save()
         response = self.client.get(
-            '/.well-known/%s' % PATH)
+            '/.well-known/%s' % VALID_PATH)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.content, DATA)
+
+    def testFetchMissingResource(self):
+        response = self.client.get(
+            '/.well-known/%s' % MISSING_PATH)
+        self.assertEqual(response.status_code, 404)
+
+    def testFailToPost(self):
+        response = self.client.post(
+            '/.well-known/%s' % VALID_PATH, {'path': 'foo'})
+        self.assertEqual(response.status_code, 403)
--- wellknown/views.py
+++ wellknown/views.py
@@ -1,14 +1,20 @@
-from django.http import HttpResponse, Http404
+from django.http import (
+    HttpResponse,
+    HttpResponseForbidden,
+    HttpResponseNotFound,
+)
 from wellknown.models import Resource
 
 
 def handle(request, path, *args, **kwargs):
+    if request.method != 'GET':
+        return HttpResponseForbidden('Only GET allowed.')
 
     try:
         r = Resource.objects.get(path=path)
         content = r.content
         content_type = r.content_type
     except Resource.DoesNotExist:
-        raise Http404('Resource %s does not exist.' % path)
+        return HttpResponseNotFound('Resource %s does not exist.' % path)
 
     return HttpResponse(content or '', content_type=content_type)
