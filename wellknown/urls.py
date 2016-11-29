d288a1cf2c74fcd2e06bc8304d0815f0badb0856
Ben Bardin <ben@altschool.com>
2016-11-29 11:39:28 -0500

    django 110 upgrade

--- wellknown/urls.py
+++ wellknown/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
+from wellknown.views import handle
 
-urlpatterns = patterns(
-    'wellknown.views',
-    url(r'^\.well-known/(?P<path>.*)', 'handle', name='wellknown'),
-)
+urlpatterns = [
+    url(r'^\.well-known/(?P<path>.*)', handle, name='wellknown'),
+]
